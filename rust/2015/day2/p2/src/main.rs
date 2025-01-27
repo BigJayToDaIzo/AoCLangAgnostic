790503c589c058a88ac8f86af9d7ac303399c2a8
Joseph Myers <josephlmyers@gmail.com>
2025-01-26 23:16:17 -0600

    finish rust day 3 2015

--- rust/2015/day2/p2/src/main.rs
+++ rust/2015/day2/p2/src/main.rs
@@ -6,26 +6,26 @@ fn main() {
     // break this thing down into package dimensions
     // let's use a lines iterator!
     let package_dimensions = input.lines();
-    let mut total_ribbon_len = 0;
+    let mut total_sq_ft = 0;
 
     // now we break each line into l, w, & h
     for dimension in package_dimensions {
         // clamp into l, w, & h by splitting on x
         let d_vec: Vec<&str> = dimension.split('x').collect();
-        total_ribbon_len += trl(d_vec);
+        total_sq_ft += tsf(d_vec);
     }
-    dbg!(total_ribbon_len);
+    dbg!(total_sq_ft);
 }
 
-fn trl(v: Vec<&str>) -> u32 {
+fn tsf(v: Vec<&str>) -> u32 {
     let l: u32 = v[0].parse().unwrap();
     let w: u32 = v[1].parse().unwrap();
     let h: u32 = v[2].parse().unwrap();
-    bow_ribbon_len(l, w, h) + l * w * h
+    2 * l * w + 2 * w * h + 2 * h * l + area_smallest_side(l, w, h)
 }
 
-fn bow_ribbon_len(l: u32, w: u32, h: u32) -> u32 {
+fn area_smallest_side(l: u32, w: u32, h: u32) -> u32 {
     let mut v = [l, w, h];
     v.sort();
-    2 * v[0] + 2 * v[1]
+    v[0] * v[1]
 }
--- rust/2015/day3/p2/src/main.rs
+++ rust/2015/day3/p2/src/main.rs
@@ -1,3 +1,72 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // read in input.txt
+    let input = fs::read_to_string("../input.txt").unwrap();
+    // dbg!(input);
+    // let's create a grid map
+    let mut sled = Point { x: 0, y: 0 };
+    let mut robo_sled = Point { x: 0, y: 0 };
+    let mut grid = vec![Point { x: 0, y: 0 }];
+    let mut iter = 0;
+    for c in input.chars() {
+        // scan char
+        match c {
+            '^' => {
+                if iter % 2 == 0 {
+                    sled.y += 1;
+                } else {
+                    robo_sled.y += 1;
+                }
+                iter += 1;
+            }
+            'v' => {
+                if iter % 2 == 0 {
+                    sled.y -= 1;
+                } else {
+                    robo_sled.y -= 1;
+                }
+                iter += 1;
+            }
+            '<' => {
+                if iter % 2 == 0 {
+                    sled.x -= 1;
+                } else {
+                    robo_sled.x -= 1;
+                }
+                iter += 1;
+            }
+            '>' => {
+                if iter % 2 == 0 {
+                    sled.x += 1;
+                } else {
+                    robo_sled.x += 1;
+                }
+                iter += 1;
+            }
+            _ => (),
+        }
+        // if sled point is in vector do nothing
+        if iter % 2 == 0 {
+            if !grid.contains(&sled) {
+                grid.push(sled);
+            }
+        } else if !grid.contains(&robo_sled) {
+            grid.push(robo_sled);
+        }
+        // else insert sled point into grid
+    }
+    dbg!(grid.len());
+}
+
+#[derive(Copy, Clone)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
 }
