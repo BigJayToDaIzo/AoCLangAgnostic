3f1fcf3d8e5a0af4f8688036df34bf2f1526708a
Joseph Myers <josephlmyers@gmail.com>
2025-02-08 22:25:30 -0600

    day 5 parts 1 and 2 complete

--- rust/2017/day-05/src/lib.rs
+++ rust/2017/day-05/src/lib.rs
@@ -4,14 +4,58 @@ pub fn read_lines() -> String {
 
 pub fn part_one(inp: &str) -> String {
     // parse jumps into usize array (should be len() 1043)
-    let instructions: &[usize; 1043];
-    for line in inp.lines() {}
-
-    "".to_string()
+    let mut instructions: [i32; 1043] = [0; 1043];
+    let mut ins_index: usize = 0;
+    let mut steps = 0;
+    for (i, line) in inp.lines().enumerate() {
+        instructions[i] = line.parse().unwrap();
+    }
+    loop {
+        // set jump offset
+        let jump_offset: i32 = instructions[ins_index];
+        steps += 1;
+        // if ins_index + offset goes out of bounds, return steps
+        if ins_index as i32 + jump_offset >= instructions.len() as i32
+            || ins_index as i32 + jump_offset < 0
+        {
+            break;
+        }
+        // increment value at index
+        instructions[ins_index] += 1;
+        // jump
+        ins_index = (ins_index as i32 + jump_offset) as usize;
+    }
+    steps.to_string()
 }
 
 pub fn part_two(inp: &str) -> String {
-    "".to_string()
+    // parse jumps into usize array (should be len() 1043)
+    let mut instructions: [i32; 1043] = [0; 1043];
+    let mut ins_index: usize = 0;
+    let mut steps = 0;
+    for (i, line) in inp.lines().enumerate() {
+        instructions[i] = line.parse().unwrap();
+    }
+    loop {
+        // set jump offset
+        let jump_offset: i32 = instructions[ins_index];
+        steps += 1;
+        // if ins_index + offset goes out of bounds, return steps
+        if ins_index as i32 + jump_offset >= instructions.len() as i32
+            || ins_index as i32 + jump_offset < 0
+        {
+            break;
+        }
+        // increment value at index
+        if jump_offset >= 3 {
+            instructions[ins_index] -= 1;
+        } else {
+            instructions[ins_index] += 1;
+        }
+        // jump
+        ins_index = (ins_index as i32 + jump_offset) as usize;
+    }
+    steps.to_string()
 }
 
 #[cfg(test)]
